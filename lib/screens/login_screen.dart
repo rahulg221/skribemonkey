688664a5680a726939246bb99c45682b1b622031
rahulg221 <rdg.ghosh@gmail.com>
2024-10-05 18:49:20 -0400

    update

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -128,6 +128,7 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Image.asset('assets/images/logo2.png', width: 270, height: 270),
             Text(
               "Skribe Monkey",
               style: TextStyle(
