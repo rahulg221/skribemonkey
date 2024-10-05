bd25146a47caeb36e7a7892571b642c1c8a8f254
raytech99 <raymanatl@gmail.com>
2024-10-05 11:15:46 -0400

    auth methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://mzsakyycsslkjszjhhmg.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16c2FreXljc3Nsa2pzempoaG1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgwOTIzMzEsImV4cCI6MjA0MzY2ODMzMX0.YV_zNu8oP2lQNyz5Py-yRuH0Vdctocl_gnGfl67KWgE',
+  );
+
   runApp(const MyApp());
 }
 
